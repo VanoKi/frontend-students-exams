36399114be41b19ee3d42a43f8cb2763dd918f02
VanoKi <kirsvano@gmail.com>
2025-04-02 17:17:38 +0300

    feat: five

--- src/main.tsx
+++ src/main.tsx
@@ -1,42 +1,25 @@
 import { createRoot } from "react-dom/client"
 
-const VideoHeader = (props: { videoName: string }) => {
-  return <div>😀 {props.videoName}</div>
+type Props = {
+  city: string // 'Minsk'
+  country: string // 'Belarus'
+  coords?: string // '53.917501,27.604851'
 }
 
-const VideoContent = (props: { videoContent: string }) => {
+const App = () => {
+  return <Component city="Minsk" />
+}
+
+const Component = ({ city, country, coords }: Props) => {
   return (
     <div>
-      📼 <a href={props.videoContent}>{props.videoContent}</a>
+      <p>{city}</p>
+      <p>{country}</p>
+      <p>{coords}</p>
     </div>
   )
-}
-
-const VideoDescription = (props: { videoDescription: string }) => {
-  return <div>📑 {props.videoDescription}</div>
-}
-
-export const YoutubeVideo = (props: any) => {
-  return (
-    <div>
-      <VideoHeader videoName={video.title} />
-      <VideoContent videoContent={video.link} />
-      <VideoDescription videoDescription={video.description} />
-    </div>
-  )
-}
-
-export const App = () => {
-  const video = {
-    title: "Samurai way",
-    link: "https://www.youtube.com/watch?v=gb7gMluAeao&list=PLcvhF2Wqh7DNVy1OCUpG3i5lyxyBWhGZ8",
-    description: "Best free react-course",
-  }
-
-  return <YoutubeVideo video={video} />
 }
 
 createRoot(document.getElementById("root")!).render(<App />)
 
-// Что нужно написать вместо xxx yyy zzz, чтобы увидеть ожидаемый результат?
-// Ответ дайте через пробел, пример: a={12} b={video.id} c={'hello'}
+// Что МИНИМАЛЬНО ДОСТАТОЧНО нужно дописать в строке 10 (cтрока с ошибкой), чтобы не было ошибки
