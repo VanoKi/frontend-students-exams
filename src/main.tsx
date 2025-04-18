349b103cbe89d8a331158b48233f5a03f305ec87
VanoKi <kirsvano@gmail.com>
2025-04-18 17:25:27 +0300

    feat: nine

--- src/main.tsx
+++ src/main.tsx
@@ -2,18 +2,17 @@ import { useState } from "react"
 import { createRoot } from "react-dom/client"
 
 function UsersList() {
-  const [users, setUsers] = useState<Array<string>>(["Bob", "Alex", "Ann"])
+  const [users, setUsers] = useState<string[]>(["Bob", "Alex", "Ann"])
   const getUser = (user: string) => <li>{user}</li>
-  const usersList = users.map(XXX)
 
   return (
     <div>
       <h4>User list:</h4>
-      <ul>{usersList}</ul>
+      <ul>{users.map(getUser)}</ul>
     </div>
   )
 }
 
 createRoot(document.getElementById("root")!).render(<UsersList />)
 
-// Какую переменную необходимо указать вместо XXX, чтобы код заработал?
+// Что вернёт выражение: typeof getUser?
