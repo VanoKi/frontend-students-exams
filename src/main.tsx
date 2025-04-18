9f3f6d50beb004a8a07b83df943d6d8c20753ae7
VanoKi <kirsvano@gmail.com>
2025-04-18 17:17:54 +0300

    feat: fourth

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,39 @@
-import { createRoot } from "react-dom/client"
 import { useState } from "react"
+import { createRoot } from "react-dom/client"
 
-function UsersList() {
-  const [users, setUsers] = useState<string[]>(["Bob", "Alex", "Ann"])
+type User = {
+  id: number
+  name: string
+  age: number
+}
+
+const UsersList = () => {
+  const state = [
+    { id: 1, name: "Bob", age: 34 },
+    { id: 2, name: "Alex", age: 25 },
+    { id: 3, name: "Ann", age: 30 },
+    { id: 4, name: "John", age: 23 },
+  ]
+
+  const users = [
+    { id: 1, userName: "Bob", age: 34 },
+    { id: 2, userName: "Alex", age: 25 },
+    { id: 3, userName: "Ann", age: 30 },
+    { id: 4, userName: "John", age: 23 },
+  ]
+
+  const [usersList, setUsersList] = useState<User[]>(XXX)
 
   return (
-    <div>
-      <h4>User list:</h4>
-      <ul>
-        {users.ХХХ((user) => (
-          <li>{user}</li>
-        ))}
-      </ul>
-    </div>
+    <>
+      <h2>User list:</h2>
+      <p>Тут будет список пользователей</p>
+    </>
   )
 }
 
 createRoot(document.getElementById("root")!).render(<UsersList />)
 
-// Что надо вставить вместо ХХХ, чтобы код заработал?
+// Что надо написать вместо XXX, чтобы код работал?
+// ❗ Если мы отмапим массив, то должны увидеть данные пользователей
+// ❗ Ответ дать минимально возможным объёмом кода
