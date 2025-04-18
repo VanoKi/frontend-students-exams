a1316609331fda63689c23ea448efdc175369589
VanoKi <kirsvano@gmail.com>
2025-04-18 17:23:00 +0300

    feat: seven

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,11 @@ import { createRoot } from "react-dom/client"
 import { useState } from "react"
 
 function UsersList() {
-  const [users, setUsers] = useState<Array<string>>(["Bob", "Alex", "Ann"])
+  const [users, setUsers] = useState<string[]>(["Bob", "Alex", "Ann"])
+  console.log(Array.isArray(users))
   return <p>Тут будет список пользователей</p>
 }
 
 createRoot(document.getElementById("root")!).render(<UsersList />)
 
-// Что вернёт выражение: typeof setUsers?
+// Что вернёт выражение: Array.isArray(users)?
