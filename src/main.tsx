7482a010da9ea6aab17dfc5083129c05d85afc3b
VanoKi <kirsvano@gmail.com>
2025-05-11 16:10:09 +0300

    feat: 3 q by 3 tst

--- src/main.tsx
+++ src/main.tsx
@@ -1,23 +1,52 @@
-import { MouseEvent, useState } from "react"
+import { useState } from "react"
 import { createRoot } from "react-dom/client"
 
-const Button = () => {
-  const [tagName, setTagName] = useState<string>()
+type User = {
+  id: number
+  name: string
+  age: number
+}
 
-  const onClickHandler = (e: MouseEvent<HTMLButtonElement>) => {
-    setTagName(e.currentTarget.tagName)
-  }
+type UserProps = User & {
+  deleteUser: (id: number) => void
+}
 
+const User = ({ deleteUser, age, name, id }: UserProps) => {
   return (
-    <>
-      <p>{tagName}</p>
-      <button onClick={onClickHandler}>
-        <span>Click</span>
-      </button>
-    </>
+    <li>
+      User {name}: {age} y.o.
+      <button onClick={() => deleteUser(id)}>x</button>
+    </li>
   )
 }
 
-createRoot(document.getElementById("root")!).render(<Button />)
+const UsersList = () => {
+  const data: User[] = [
+    { id: 1, name: "Bob", age: 25 },
+    { id: 2, name: "Alex", age: 28 },
+    { id: 3, name: "Ann", age: 23 },
+    { id: 4, name: "John", age: 30 },
+  ]
 
-// Что надо написать вместо ххх, что бы на странице появился текст BUTTON?
+  const [users, setUsers] = useState<User[]>(data)
+
+  const deleteUser = (userID: number) => {
+    const filteredUsers = users.filter((u) => u.id !== userID)
+    setUsers(xxx)
+  }
+
+  return (
+    <main>
+      <h4>User list:</h4>
+      <ul>
+        {users.map((u) => (
+          <User key={u.id} {...u} deleteUser={deleteUser} />
+        ))}
+      </ul>
+    </main>
+  )
+}
+
+createRoot(document.getElementById("root")!).render(<UsersList />)
+
+// Что надо написать вместо xxx, чтобы код работал?
