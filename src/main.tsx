89d8d5dfc085d2c3aefbf24789e779d7b19e004b
VanoKi <kirsvano@gmail.com>
2025-05-11 16:16:31 +0300

    feat: 7 by 3

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,12 @@ type User = {
   age: number
 }
 
+const User = (props: User) => (
+  <li>
+    Student {props.name}: {props.age} y.o.
+  </li>
+)
+
 const UsersList = () => {
   const data: User[] = [
     { id: 1, name: "Bob", age: 34 },
@@ -14,23 +20,21 @@ const UsersList = () => {
     { id: 3, name: "Ann", age: 25 },
     { id: 4, name: "John", age: 36 },
   ]
+
   const [users] = useState<User[]>(data)
 
   return (
     <main>
       <h4>User list:</h4>
       <ul>
-        {users.map((u) => {
-          return (
-            <li xxx={u.id}>
-              Student {u.name}: {u.age} y.o.
-            </li>
-          )
-        })}
+        {users.map((u) => (
+          <User key={u.id} {...xxx} />
+        ))}
       </ul>
     </main>
   )
 }
 
 createRoot(document.getElementById("root")!).render(<UsersList />)
-// Что надо написать вместо xxx, чтобы код работал оптимально?
+
+// Что надо написать вместо xxx, чтобы код работал?
