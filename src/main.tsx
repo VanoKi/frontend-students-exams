10ff9e36e39c517c4dd2fc4d5c87dfac8e66aa32
VanoKi <kirsvano@gmail.com>
2025-05-11 16:49:54 +0300

    feat: 8 by 4

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,36 @@
-import { useState } from "react"
+import { useState, ChangeEvent } from "react"
 import { createRoot } from "react-dom/client"
 
-const User = () => {
-  const [userName, setUserName] = useState<string>("")
+const Notes = () => {
+  const [newNote, setNewNote] = useState<string>("")
+  const [notes, setNotes] = useState<string[]>([])
 
-  const onChangeHandler = (e: xxx) => {
-    setUserName(e.currentTarget.value)
+  const onChangeHandler = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    setNewNote(e.currentTarget.value)
+  }
+
+  const addNoteHandler = () => {
+    setNotes([newNote, ...notes])
+    setNewNote("")
   }
 
   return (
     <div>
-      <p>{userName}</p>
-      <input onChange={onChangeHandler} />
+      <textarea value={newNote} onChange={onChangeHandler} />
+      <div>
+        <button onClick={addNoteHandler}>Add note</button>
+        <button onClick={xxx}>Clear notes list</button>
+      </div>
+      <h4>Notes:</h4>
+      <div>
+        {notes.map((note) => (
+          <p>{note}</p>
+        ))}
+      </div>
     </div>
   )
 }
-createRoot(document.getElementById("root")!).render(<User />)
 
-// Что надо написать вместо ххх, чтобы правильно типизировать параметр функции?
+createRoot(document.getElementById("root")!).render(<Notes />)
+// Что надо написать вместо ххх,
+// чтобы при клике на кнопку `Clear notes list` список заметок очищался?
