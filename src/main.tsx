e0b2205a901296582f972b7cff99fcefd1abe3b1
VanoKi <kirsvano@gmail.com>
2025-05-11 16:24:57 +0300

    feat: 11 by 3

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,41 @@
+import { useState } from "react"
 import { createRoot } from "react-dom/client"
 
-function Button() {
-  return <button onClick={xxx}>Click</button>
+type User = {
+  id: number
+  name: string
+  age: number
 }
 
-createRoot(document.getElementById("root")!).render(<Button />)
+const UsersList = () => {
+  const data: User[] = [
+    { id: 1, name: "Bob", age: 24 },
+    { id: 2, name: "Alex", age: 28 },
+    { id: 3, name: "Ann", age: 23 },
+    { id: 4, name: "John", age: 30 },
+  ]
 
-// Что надо написать вместо ххх,
-// чтобы на странице появился пустой alert при клике по кнопке?
+  const [users] = useState<User[]>(data)
+
+  // Пользователи старше 25 лет:
+  const olderThen25Users = users.xxx((u) => u.age > 25)
+
+  return (
+    <main>
+      <h4>User list:</h4>
+      <ul>
+        {olderThen25Users.map((u) => {
+          return (
+            <li key={u.id}>
+              User {u.name}: {u.age} y.o.
+            </li>
+          )
+        })}
+      </ul>
+    </main>
+  )
+}
+
+createRoot(document.getElementById("root")!).render(<UsersList />)
+
+// Что надо написать вместо xxx, чтобы отрисовались пользователи старше 25 лет?
