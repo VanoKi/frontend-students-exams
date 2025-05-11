ad7c81f155c2d3727d56b1c681f4c1898bbd3d45
VanoKi <kirsvano@gmail.com>
2025-05-11 16:13:04 +0300

    feat: 5 by 3

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,13 @@
-export const user = {}
-export const cash = user
-export const student = {}
+import { MouseEvent } from "react"
+import { createRoot } from "react-dom/client"
 
-if (XXX === user) {
-  console.log("Yo!!!")
+const Button = () => {
+  const onClickHandler = (e: MouseEvent<HTMLButtonElement>) => {
+    console.log(typeof e === ххх)
+  }
+  return <button onClick={onClickHandler}>Click</button>
 }
 
-// Какую переменную надо указать вместо XXX, кроме user, что бы в консоль вывелась строка "Yo!!!?
+createRoot(document.getElementById("root")!).render(<Button />)
+
+// Что надо написать вместо ххх, чтобы в консоль вывело true?
