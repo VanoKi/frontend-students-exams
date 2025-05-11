12ddb1bb25c3773480d61d02d78f656e459a81e1
VanoKi <kirsvano@gmail.com>
2025-05-11 16:15:46 +0300

    feat: 6 by 3

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,36 @@
-import { MouseEvent } from "react"
+import { useState } from "react"
 import { createRoot } from "react-dom/client"
 
-const Button = () => {
-  const onClickHandler = (e: MouseEvent<HTMLButtonElement>) => {
-    console.log(typeof e === ххх)
-  }
-  return <button onClick={onClickHandler}>Click</button>
+type User = {
+  id: number
+  name: string
+  age: number
 }
 
-createRoot(document.getElementById("root")!).render(<Button />)
+const UsersList = () => {
+  const data: User[] = [
+    { id: 1, name: "Bob", age: 34 },
+    { id: 2, name: "Alex", age: 29 },
+    { id: 3, name: "Ann", age: 25 },
+    { id: 4, name: "John", age: 36 },
+  ]
+  const [users] = useState<User[]>(data)
 
-// Что надо написать вместо ххх, чтобы в консоль вывело true?
+  return (
+    <main>
+      <h4>User list:</h4>
+      <ul>
+        {users.map((u) => {
+          return (
+            <li xxx={u.id}>
+              Student {u.name}: {u.age} y.o.
+            </li>
+          )
+        })}
+      </ul>
+    </main>
+  )
+}
+
+createRoot(document.getElementById("root")!).render(<UsersList />)
+// Что надо написать вместо xxx, чтобы код работал оптимально?
