de36a3ba9c8ef9ff2655f4cd1f52e08ae3a98211
VanoKi <kirsvano@gmail.com>
2025-05-12 18:41:39 +0300

    feat: 6 by 2.3

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,35 @@
-const names = ["Igor", "Konstantin", "Masha"]
+type User = {
+  id: number
+  name: string
+  status: "online" | "offline"
+}
 
-const nameNext = names
+type Address = {
+  country: string
+  city: string
+  email: string
+}
 
-nameNext.length = 0
+type Adresses = {
+  [userID: string]: Address
+}
 
-export const result = names[0] ? 66 : 39
+const users: User[] = [
+  { id: 1, name: "Bob", status: "online" },
+  { id: 2, name: "Alex", status: "offline" },
+  { id: 3, name: "Ann", status: "offline" },
+]
 
-// Какое значение получит переменная result?
+export const addresses: Adresses = {
+  1: { country: "Russia", city: "Moscow", email: "hey@email.com" },
+  2: { country: "Ukraine", city: "Kiev", email: "yo@send.ua" },
+  3: { country: "Belarus", city: "Minsk", email: "wow@gogo.ru" },
+}
+
+export const changeUserStatus = (userID: number, status: string) => {
+  return users.xxx((u) => (u.id === userID ? { ...u, status } : u))
+}
+
+// Дан список пользователей и структура, хранящая адреса пользователей.
+// Так же дана функция changeUserStatus, которая меняет статус пользователя.
+// Что надо написать вместо ххх, чтобы функция работала корректно?
