fe00bf35258759db61930880e1f43168b29ef318
VanoKi <kirsvano@gmail.com>
2025-05-12 18:37:43 +0300

    feat: 2 by 2.3

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,60 @@
-const customer = {
-  name: "Maria",
-  age: 21,
-  scores: [6.57, 51.25, 45.51],
+type Samurai = {
+  id: string
+  name: string
+  status: "online" | "offline"
 }
 
-export const [first, second, third = 31.41] = customer.scores
+type CourseName = "HTML" | "JS" | "React" | "Redux" | "HomeWorks"
 
-switch (third) {
-  case 31.41:
-    console.log("Italy")
-    break
-  case 51.25:
-    console.log("Germany")
-    break
-  default:
-    console.log("Mongolia")
+type Course = {
+  name: CourseName
+  mentor: string
+  isDone: boolean
 }
 
-// Какую строку мы увидим в консоли?
+type Technologies = {
+  [userID: string]: Course[]
+}
+
+const samuraiID_1 = "64jf-87kg"
+const samuraiID_2 = "90lg-34ks"
+const samuraiID_3 = "12jm-05fd"
+
+export const samurai: Samurai[] = [
+  { id: samuraiID_1, name: "Bob", status: "online" },
+  { id: samuraiID_2, name: "Alex", status: "offline" },
+  { id: samuraiID_3, name: "Ann", status: "offline" },
+]
+
+export const technologies: Technologies = {
+  [samuraiID_1]: [
+    { name: "HTML", mentor: "Svetlana", isDone: true },
+    { name: "JS", mentor: "Viktor", isDone: true },
+    { name: "React", mentor: "Dmitrij", isDone: false },
+    { name: "Redux", mentor: "Valera", isDone: false },
+    { name: "HomeWorks", mentor: "Ignat", isDone: true },
+  ],
+  [samuraiID_2]: [
+    { name: "HTML", mentor: "Svetlana", isDone: true },
+    { name: "JS", mentor: "Viktor", isDone: true },
+    { name: "React", mentor: "Dmitrij", isDone: true },
+    { name: "Redux", mentor: "Valera", isDone: false },
+    { name: "HomeWorks", mentor: "Ignat", isDone: true },
+  ],
+  [samuraiID_3]: [
+    { name: "HTML", mentor: "Svetlana", isDone: true },
+    { name: "JS", mentor: "Viktor", isDone: true },
+    { name: "React", mentor: "Dmitrij", isDone: false },
+    { name: "Redux", mentor: "Valera", isDone: false },
+    { name: "HomeWorks", mentor: "Ignat", isDone: false },
+  ],
+}
+
+export const updateCourseStatus = (samuraiID: string, name: CourseName, isDone: boolean) => {
+  return { ...technologies, [samuraiID]: technologies[xxx].map((c) => (c.name === name ? { ...c, isDone } : c)) }
+}
+// Дан список самураев из инкубатора и структура, хранящая данные о курсах,
+// которые самурай уже прошёл, а так же о тех курсах, которые ему ещё предстоит пройти.
+// Так же дана функция updateCourseStatus,
+// которая позволяет отметить курс как пройденный самураем или снять такую отметку
+// Что надо написать вместо ххх, чтобы функция работала корректно?
