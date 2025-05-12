e3668ba61bf28c535170bd501caeded542b888d4
VanoKi <kirsvano@gmail.com>
2025-05-12 14:05:58 +0300

    feat: 3 by total1

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,36 @@
+import { useState, ChangeEvent } from "react"
 import { createRoot } from "react-dom/client"
-import { useState } from "react"
 
-function UsersList() {
-  const [users, setUsers] = useState<Array<string>>(["Bob", "Alex", "Ann"])
-  console.log(typeof setUsers)
-  return <p>Тут будет список пользователей</p>
+const Notes = () => {
+  const [newNote, setNewNote] = useState<string>("")
+  const [notes, setNotes] = useState<string[]>([])
+
+  const onChangeHandler = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    setNewNote(e.currentTarget.value)
+  }
+
+  const addNoteHandler = () => {
+    setNotes([newNote, ...notes])
+    setNewNote("")
+  }
+
+  return (
+    <div>
+      <textarea value={newNote} onChange={onChangeHandler} />
+      <div>
+        <button onClick={addNoteHandler}>Add note</button>
+        <button onClick={xxx}>Clear notes list</button>
+      </div>
+      <h4>Notes:</h4>
+      <div>
+        {notes.map((note) => (
+          <p>{note}</p>
+        ))}
+      </div>
+    </div>
+  )
 }
 
-createRoot(document.getElementById("root")!).render(<UsersList />)
-
-// Что вернёт выражение: typeof setUsers?
+createRoot(document.getElementById("root")!).render(<Notes />)
+// Что надо написать вместо ххх,
+// чтобы при клике на кнопку `Clear notes list` список заметок очищался?
