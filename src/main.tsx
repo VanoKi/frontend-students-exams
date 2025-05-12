ba8df021063ae59c07c9449c8b2da71a91893430
VanoKi <kirsvano@gmail.com>
2025-05-12 18:24:38 +0300

    feat: 4 by 2.2

--- src/main.tsx
+++ src/main.tsx
@@ -1,29 +1,8 @@
-type Student = {
-  id: number
-  name: string
-  age: number
-}
+const members = ["Leonid", "Nikita", "Lev"]
+const customer = [...members]
+const answer = customer
 
-type Friends = {
-  [key: string]: string[]
-}
+export const result = answer !== XXX ? "Yo!!!" : "Hey!!!"
 
-export const students: Student[] = [
-  { id: 1, name: "Bob", age: 34 },
-  { id: 2, name: "Alex", age: 23 },
-  { id: 3, name: "Ann", age: 25 },
-  { id: 4, name: "Charley", age: 20 },
-]
-
-export const friends: Friends = {
-  1: ["Jack", "Oliver", "Oscar"],
-  2: ["Jack", "Lewis", "Thomas"],
-  3: ["William", "Lewis", "Michael"],
-  4: ["Oscar", "Thomas", "William"],
-}
-
-//Дан массив студентов и структура,
-//которая содержит список друзей каждого из студентов.
-//Id студента является ключом к массиву его друзей.
-//Имеют ли студенты students[1] и students[3] общих друзей?
-//Если да, напишите в ответе имя общего друга в кавычках, если нет - напишите в ответе false.
+// Имя какой переменной надо указать вместо XXX, что бы переменная result получила значение "Yo!!!"?
+// Если такой переменной нет, в качестве ответа укажите null.
