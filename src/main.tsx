fe2afde3559c29a027a764bf93f1c47622c2ac8a
VanoKi <kirsvano@gmail.com>
2025-05-12 14:11:37 +0300

    feat: 7 by total1

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,31 @@
-import { useState } from "react"
-import { createRoot } from "react-dom/client"
+import ReactDOM from "react-dom/client"
+import React from "react"
 
-const User = () => {
-  const [userName, setUserName] = useState<string>("")
+type PagePropsType = {
+  age: number
+  name: string
+  avatarUrl: string
+}
 
+const Page: React.FC<PagePropsType> = (props) => {
+  return <User a={xxx} n={yyy} />
+}
+
+type UserPropsType = {
+  a: number
+  n: string
+}
+
+export const User: React.FC<UserPropsType> = (props) => {
   return (
     <div>
-      <p>{userName}</p>
-      <input
-        // ❗X
-        onChange={(e) => setUserName(e.currentTarget.value)}
-      />
+      name: {props.n}, age: {props.a}
     </div>
   )
 }
 
-createRoot(document.getElementById("root")!).render(<User />)
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
+root.render(<Page age={40} name="Brendan" avatarUrl={"test"} />)
 
-// Что надо написать вместо ❗X, чтобы input был контролируемым?
+// Что нужно написать вместо xxx и yyy?
+// Ответ дайте через пробел, например: blabla onClick(props)
