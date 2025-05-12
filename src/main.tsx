c64e781401909d1a882c557dff5d746b1c4a243e
VanoKi <kirsvano@gmail.com>
2025-05-12 18:49:30 +0300

    feat: 10 by 2.3

--- src/main.tsx
+++ src/main.tsx
@@ -1,36 +1,11 @@
-type User = {
-  id: number
-  name: string
-  status: "online" | "offline"
+const guest = {
+  name: "Eva",
+  age: 49,
+  scores: [82.69, 88.85, 25.47],
 }
 
-type Address = {
-  country: string
-  city: string
-  email: string
-}
+export const { name, age, scores } = guest
 
-type Adresses = {
-  [userID: string]: Address
-}
+console.log(guest.scores === scores && age)
 
-export const users: User[] = [
-  { id: 1, name: "Bob", status: "online" },
-  { id: 2, name: "Alex", status: "offline" },
-  { id: 3, name: "Ann", status: "offline" },
-]
-
-const addresses: Adresses = {
-  1: { country: "Russia", city: "Moscow", email: "hey@email.com" },
-  2: { country: "Ukraine", city: "Kiev", email: "yo@send.ua" },
-  3: { country: "Belarus", city: "Minsk", email: "wow@gogo.ru" },
-}
-
-export const updateUserAddress = (userID: number, key: string, newValue: string) => {
-  return { ...addresses, [userID]: { ...addresses[xxx], [key]: newValue } }
-}
-// Дан список пользователей и структура, хранящая адреса пользователей.
-// Так же дана функция updateUserAddress,
-// которая обновляет указанное в параметрах поле в адресе пользователя.
-// Пример использования функции: updateUserAddress(2, "city", "Dnepropetrovsk")
-// Что надо написать вместо ххх, чтобы функция работала корректно?
+// Какое значение мы увидим в консоли?
