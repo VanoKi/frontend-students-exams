40bac1cfd493cda2722591bd48de702705a3d640
VanoKi <kirsvano@gmail.com>
2025-05-12 18:44:48 +0300

    feat: 8 by 2.3

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,37 @@
-const getArray = (a, b, c) => {
-  return [c, b, a]
+type Student = {
+  id: number
+  name: string
 }
 
-export const [beginning, business, course] = getArray(58, 70, 15)
+type Friends = {
+  [key: string]: string[]
+}
 
-console.log(beginning)
+export const students: Student[] = [
+  { id: 1, name: "Bob" },
+  { id: 2, name: "Alex" },
+  { id: 3, name: "Ann" },
+  { id: 4, name: "Charley" },
+]
 
-// Какое число будет выведено в консоль?
+export const friends: Friends = {
+  1: ["Oliver", "Jack", "Oscar"],
+  2: ["Jack", "Lewis", "Thomas"],
+  3: ["William", "Michael", "Lewis"],
+  4: ["Oscar", "James", "William"],
+}
+
+const getMutualFriends = (st_1: Student, st_2: Student) => {
+  const result: string[] = []
+  friends[st_1.id].forEach((f) => (friends[xxx].includes(f) ? result.push(f) : null))
+  return result
+}
+
+// Дан массив студентов и структура,
+// которая содержит список друзей каждого из студентов.
+// Так же дана функция getMutualFriends, проверяющая наличие общих друзей
+// у двух выбранных студентов.
+// Функция принимает параметром два объекта типа Student
+// и возвращает массив с именами общих друзей,
+// если они есть и пустой массив, если таковых нету.
+// Что надо написать вместо ххх, чтобы функция работала?
