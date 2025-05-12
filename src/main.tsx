24ac0887b740eaa2c6d46fe88d944a0e07c6e18f
VanoKi <kirsvano@gmail.com>
2025-05-12 14:16:00 +0300

    feat: 10 by total1

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,39 @@
 import { useState } from "react"
 import { createRoot } from "react-dom/client"
 
-function UsersList() {
-  const results = useState<string[]>(["Bob", "Alex", "Ann"])
+type User = {
+  id: number
+  name: string
+  age: number
+}
 
-  const users = results[0]
-  const setUsers = results[1]
+const UsersList = () => {
+  const state = [
+    { id: 1, name: "Bob", age: 34 },
+    { id: 2, name: "Alex", age: 25 },
+    { id: 3, name: "Ann", age: 30 },
+    { id: 4, name: "John", age: 23 },
+  ]
 
-  return <p>Тут будет список пользователей</p>
+  const users = [
+    { id: 1, userName: "Bob", age: 34 },
+    { id: 2, userName: "Alex", age: 25 },
+    { id: 3, userName: "Ann", age: 30 },
+    { id: 4, userName: "John", age: 23 },
+  ]
+
+  const [usersList, setUsersList] = useState<User[]>(XXX)
+
+  return (
+    <>
+      <h2>User list:</h2>
+      <p>Тут будет список пользователей</p>
+    </>
+  )
 }
 
 createRoot(document.getElementById("root")!).render(<UsersList />)
 
-// Чему равно results.length?
+// Что надо написать вместо XXX, чтобы код работал?
+// ❗ Если мы отмапим массив, то должны увидеть данные пользователей
+// ❗ Ответ дать минимально возможным объёмом кода
