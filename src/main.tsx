bfddb14f2c02dd278ed69807f80419c71f41307e
VanoKi <kirsvano@gmail.com>
2025-05-12 18:36:56 +0300

    feat: 1 by 2.3

--- src/main.tsx
+++ src/main.tsx
@@ -1,29 +1,20 @@
-type Student = {
-  id: number
-  name: string
+const customer = {
+  name: "Maria",
+  age: 21,
+  scores: [6.57, 51.25, 45.51],
 }
 
-type Students = Student[]
+export const [first, second, third = 31.41] = customer.scores
 
-type Friends = {
-  [key: string]: string[]
+switch (third) {
+  case 31.41:
+    console.log("Italy")
+    break
+  case 51.25:
+    console.log("Germany")
+    break
+  default:
+    console.log("Mongolia")
 }
 
-export const students: Students = [
-  { id: 1, name: "Bob" },
-  { id: 2, name: "Alex" },
-  { id: 3, name: "Ann" },
-  { id: 4, name: "Charley" },
-]
-
-export const friends: Friends = {
-  1: ["Oliver", "Jack", "Oscar"],
-  2: ["Jack", "Lewis", "Thomas"],
-  3: ["William", "Michael", "Lewis"],
-  4: ["Oscar", "James", "William"],
-}
-
-//Дан список студентов и структура,
-//которая содержит список друзей каждого из студентов.
-//Id студента является ключом к массиву его друзей.
-//Что вернёт выражение: Array.isArray(friends)?
+// Какую строку мы увидим в консоли?
