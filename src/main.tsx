7eb9dedbd2a6527a917aa65d535ec5884258b16f
VanoKi <kirsvano@gmail.com>
2025-05-12 16:33:13 +0300

    feat: 4 by 2.1

--- src/main.tsx
+++ src/main.tsx
@@ -1,43 +1,10 @@
-import { ChangeEvent, useState } from "react"
-import { createRoot } from "react-dom/client"
-
-const MIN_COMMENT_SIZE = 5
-
-const LongCommentChecker = () => {
-  const [comment, setComment] = useState<string>("")
-
-  const isCommentReady = comment.length > MIN_COMMENT_SIZE
-
-  const setCommentHandler = (e: ChangeEvent<HTMLTextAreaElement>) => {
-    const newComment = e.currentTarget.value
-
-    setComment(newComment)
-  }
-
-  const clearCommentsHandler = () => {
-    if (isCommentReady) {
-      setComment("")
-    }
-  }
-
-  return (
-    <main>
-      <textarea
-        placeholder={"Your comment must have more than 5 charters"}
-        value={comment}
-        onChange={setCommentHandler}
-      />
-      <div>
-        <button disabled={XXX} onClick={clearCommentsHandler}>
-          Send comment
-        </button>
-      </div>
-    </main>
-  )
+const client = {
+  name: "Ivan",
+  age: 82,
 }
 
-createRoot(document.getElementById("root")!).render(<LongCommentChecker />)
+export const copyClient = client
 
-// Что нужно написать вместо XXX, чтобы кнопка отправки комментария отрабатывала верно:
-// первоначально кнопка должна быть в состоянии disable, а после успешного выполнения условия (комментарий должен быть больше 5 символов) должна раздизаблиться.
-// ❗ Ответ необходимо дать на основании данных (переменных), которые уже есть в коде
+client.age = 71
+
+// Чему равно значение copyClient.age после выполнения этого кода?
