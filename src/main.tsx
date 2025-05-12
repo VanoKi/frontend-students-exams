c67cec42d39eb51082f6296c164067ad7550ec70
VanoKi <kirsvano@gmail.com>
2025-05-12 14:07:28 +0300

    feat: 4 by total1

--- src/main.tsx
+++ src/main.tsx
@@ -1,36 +1,19 @@
-import { useState, ChangeEvent } from "react"
 import { createRoot } from "react-dom/client"
 
-const Notes = () => {
-  const [newNote, setNewNote] = useState<string>("")
-  const [notes, setNotes] = useState<string[]>([])
+type UserProps = {
+  name: string
+  description: string
+}
 
-  const onChangeHandler = (e: ChangeEvent<HTMLTextAreaElement>) => {
-    setNewNote(e.currentTarget.value)
-  }
-
-  const addNoteHandler = () => {
-    setNotes([newNote, ...notes])
-    setNewNote("")
-  }
-
+const User = (props: UserProps) => {
   return (
     <div>
-      <textarea value={newNote} onChange={onChangeHandler} />
-      <div>
-        <button onClick={addNoteHandler}>Add note</button>
-        <button onClick={xxx}>Clear notes list</button>
-      </div>
-      <h4>Notes:</h4>
-      <div>
-        {notes.map((note) => (
-          <p>{note}</p>
-        ))}
-      </div>
+      <p>Имя: {props.name}</p>
+      <p>Описание: {"❗X"}</p>
     </div>
   )
 }
 
-createRoot(document.getElementById("root")!).render(<Notes />)
-// Что надо написать вместо ххх,
-// чтобы при клике на кнопку `Clear notes list` список заметок очищался?
+createRoot(document.getElementById("root")!).render(<User name={"John"} description={"lorem ipsum"} />)
+
+// Что нужно написать вместо '❗X', что бы в браузере отобразилось описание, которое передали в пропсах ?
