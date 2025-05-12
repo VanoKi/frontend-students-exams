ecdc237632c9592d5577bf2593f813917f1abf5b
VanoKi <kirsvano@gmail.com>
2025-05-12 18:39:41 +0300

    feat: 3 by 2.3

--- src/main.tsx
+++ src/main.tsx
@@ -1,60 +1,9 @@
-type Samurai = {
-  id: string
-  name: string
-  status: "online" | "offline"
+const userName = (user = "") => {
+  let userName: any = "Nikita"
+  userName += user
+  return user
 }
 
-type CourseName = "HTML" | "JS" | "React" | "Redux" | "HomeWorks"
+export const student = userName() || "Ekaterina"
 
-type Course = {
-  name: CourseName
-  mentor: string
-  isDone: boolean
-}
-
-type Technologies = {
-  [userID: string]: Course[]
-}
-
-const samuraiID_1 = "64jf-87kg"
-const samuraiID_2 = "90lg-34ks"
-const samuraiID_3 = "12jm-05fd"
-
-export const samurai: Samurai[] = [
-  { id: samuraiID_1, name: "Bob", status: "online" },
-  { id: samuraiID_2, name: "Alex", status: "offline" },
-  { id: samuraiID_3, name: "Ann", status: "offline" },
-]
-
-export const technologies: Technologies = {
-  [samuraiID_1]: [
-    { name: "HTML", mentor: "Svetlana", isDone: true },
-    { name: "JS", mentor: "Viktor", isDone: true },
-    { name: "React", mentor: "Dmitrij", isDone: false },
-    { name: "Redux", mentor: "Valera", isDone: false },
-    { name: "HomeWorks", mentor: "Ignat", isDone: true },
-  ],
-  [samuraiID_2]: [
-    { name: "HTML", mentor: "Svetlana", isDone: true },
-    { name: "JS", mentor: "Viktor", isDone: true },
-    { name: "React", mentor: "Dmitrij", isDone: true },
-    { name: "Redux", mentor: "Valera", isDone: false },
-    { name: "HomeWorks", mentor: "Ignat", isDone: true },
-  ],
-  [samuraiID_3]: [
-    { name: "HTML", mentor: "Svetlana", isDone: true },
-    { name: "JS", mentor: "Viktor", isDone: true },
-    { name: "React", mentor: "Dmitrij", isDone: false },
-    { name: "Redux", mentor: "Valera", isDone: false },
-    { name: "HomeWorks", mentor: "Ignat", isDone: false },
-  ],
-}
-
-export const updateCourseStatus = (samuraiID: string, name: CourseName, isDone: boolean) => {
-  return { ...technologies, [samuraiID]: technologies[xxx].map((c) => (c.name === name ? { ...c, isDone } : c)) }
-}
-// Дан список самураев из инкубатора и структура, хранящая данные о курсах,
-// которые самурай уже прошёл, а так же о тех курсах, которые ему ещё предстоит пройти.
-// Так же дана функция updateCourseStatus,
-// которая позволяет отметить курс как пройденный самураем или снять такую отметку
-// Что надо написать вместо ххх, чтобы функция работала корректно?
+// Какое значение получит переменная student?
