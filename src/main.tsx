c1986b0a25c1a03ee25660d847201d110402e47e
VanoKi <kirsvano@gmail.com>
2025-05-12 18:44:16 +0300

    feat: 7 by 2.3

--- src/main.tsx
+++ src/main.tsx
@@ -1,35 +1,9 @@
-type User = {
-  id: number
-  name: string
-  status: "online" | "offline"
+const getArray = (a, b, c) => {
+  return [c, b, a]
 }
 
-type Address = {
-  country: string
-  city: string
-  email: string
-}
+export const [beginning, business, course] = getArray(58, 70, 15)
 
-type Adresses = {
-  [userID: string]: Address
-}
+console.log(beginning)
 
-const users: User[] = [
-  { id: 1, name: "Bob", status: "online" },
-  { id: 2, name: "Alex", status: "offline" },
-  { id: 3, name: "Ann", status: "offline" },
-]
-
-export const addresses: Adresses = {
-  1: { country: "Russia", city: "Moscow", email: "hey@email.com" },
-  2: { country: "Ukraine", city: "Kiev", email: "yo@send.ua" },
-  3: { country: "Belarus", city: "Minsk", email: "wow@gogo.ru" },
-}
-
-export const changeUserStatus = (userID: number, status: string) => {
-  return users.xxx((u) => (u.id === userID ? { ...u, status } : u))
-}
-
-// Дан список пользователей и структура, хранящая адреса пользователей.
-// Так же дана функция changeUserStatus, которая меняет статус пользователя.
-// Что надо написать вместо ххх, чтобы функция работала корректно?
+// Какое число будет выведено в консоль?
