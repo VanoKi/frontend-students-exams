74c992f9a49c726e031587161d89d708674d082f
VanoKi <kirsvano@gmail.com>
2025-06-17 13:25:47 +0300

    feat: total 2 4

--- src/main.tsx
+++ src/main.tsx
@@ -1,36 +1,24 @@
-type User = {
-  id: number
-  name: string
-  status: "online" | "offline"
+type Action = {
+  type: "SUM" | "SUB" | "MULT" | "DIV"
+  payload: number
 }
 
-type Address = {
-  country: string
-  city: string
-  email: string
+export const calculator = (state: number, action: Action): number => {
+  switch (action.type) {
+    case "SUM":
+      return state + action.payload
+    case "SUB":
+      return state - action.payload
+    case "DIV":
+      return state / action.payload
+    case "MULT":
+      return state * action.payload
+    default:
+      return state
+  }
 }
 
-type Adresses = {
-  [userID: string]: Address
-}
+const result = calculator(10, { XXX, payload: 5 })
+console.log(result)
 
-export const users: User[] = [
-  { id: 1, name: "Bob", status: "online" },
-  { id: 2, name: "Alex", status: "offline" },
-  { id: 3, name: "Ann", status: "offline" },
-]
-
-const addresses: Adresses = {
-  1: { country: "Russia", city: "Moscow", email: "hey@email.com" },
-  2: { country: "Ukraine", city: "Kiev", email: "yo@send.ua" },
-  3: { country: "Belarus", city: "Minsk", email: "wow@gogo.ru" },
-}
-
-export const updateUserAddress = (userID: number, key: string, newValue: string) => {
-  return { ...addresses, [userID]: { ...addresses[xxx], [key]: newValue } }
-}
-// Дан список пользователей и структура, хранящая адреса пользователей.
-// Так же дана функция updateUserAddress,
-// которая обновляет указанное в параметрах поле в адресе пользователя.
-// Пример использования функции: updateUserAddress(2, "city", "Dnepropetrovsk")
-// Что надо написать вместо ххх, чтобы функция работала корректно?
+//Что надо написать вместо XXX, что бы переменная result содержала значение 5?
