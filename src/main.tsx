f2f7529940dca744fa95a3f2526820eb56aed0ce
VanoKi <kirsvano@gmail.com>
2025-06-17 12:55:43 +0300

    feat: 2.4 7

--- src/main.tsx
+++ src/main.tsx
@@ -1,27 +1,13 @@
-type Action = {
-  type: "SUM" | "SUB" | "MULT" | "DIV" | "EXP"
-  payload: number
+const community = {
+  name: "Michail",
+  scores: 6,
 }
 
-export const calculator = (state: number, action: Action): number => {
-  switch (action.type) {
-    case "SUM":
-      return state + action.payload
-    case "SUB":
-      return state - action.payload
-    case "DIV":
-      return state / action.payload
-    case "MULT":
-      return state * action.payload
-    case "EXP":
-      return state ** action.payload
-    default:
-      return state
-  }
-}
-const result = calculator(10, { XXX, payload: 0 })
-if (!(result - 1)) {
-  console.log("IT-INCUBATOR")
+const basis = {
+  ...community,
+  scores: community.scores++,
 }
 
-// Что надо написать вместо XXX чтобы в консоли появилась строка "IT-INCUBATOR"?
+export const scores = basis.scores
+
+// Какое значение получит переменная scores?
