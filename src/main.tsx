cc90d3a8a7f0d82b71928794c4c357842bdd5ce1
VanoKi <kirsvano@gmail.com>
2025-06-18 18:27:13 +0300

    feat: 3.4

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,28 @@
-const array = [1, 2, 3, 4, 5]
-const result = array.push(6) - array.pop()
+const reducer = (state: any, action: any) => {
+  switch (action.type) {
+    case "TRACK-LIKED":
+      return {
+        ...state,
+        [XXX]: {
+          ...state[XXX],
+          likesCount: state[XXX].likesCount + 1,
+        },
+      }
+    default:
+      return state
+  }
+}
 
-export const bestStudent = result ? "Lev" : "Rita"
+const likeTrackAC = (trackId: number) => ({ type: "TRACK-LIKED", trackId })
 
-// Какое значение получит переменная bestStudent?
+const state = {
+  12: { id: 12, likesCount: 10 },
+  14: { id: 14, likesCount: 2 },
+  100: { id: 100, likesCount: 0 },
+}
+const newState = reducer(state, likeTrackAC(14))
+
+console.log(newState[14].likesCount === 3)
+
+// Что нужно написать вместо XXX, чтобы в консоли увидеть true?
+// ❗ Захардкодженные значения использовать запрещено
