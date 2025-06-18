13914b446734f7fa21188fea2b9d6212874234a2
VanoKi <kirsvano@gmail.com>
2025-06-18 18:33:21 +0300

    feat: 3.6

--- src/main.tsx
+++ src/main.tsx
@@ -7,12 +7,12 @@ type PlayerState = {
   status: Status
 }
 
-const playerReducer = (state: PlayerState, action: any) => {
+export const playerReducer = (state: PlayerState, action: any) => {
   switch (action.type) {
-    case "TRACK-VOLUME-CHANGED":
+    case XXX:
       return {
         ...state,
-        volume: action.volumeLevel,
+        trackUrl: action.url,
       }
     default:
       return state
@@ -20,17 +20,7 @@ const playerReducer = (state: PlayerState, action: any) => {
 }
 
 export const muteTrackAC = () => ({ type: "TRACK-MUTED" })
-export const changeVolumeAC = (volumeLevel: number) => ({ type: "TRACK-VOLUME-CHANGED", volumeLevel })
 export const changeTrackAC = (url: string) => ({ type: "TRACK-URL-CHANGED", url })
 export const changeTrackPlayStatusAC = (status: Status) => ({ type: "TRACK-STATUS-CHANGED", status })
 
-const state: PlayerState = {
-  status: "Stopped",
-  currentPlayPosition: 1213,
-  trackUrl: "https://blabla.com/track01.mp3",
-  volume: 100,
-}
-const newState = playerReducer(state, changeVolumeAC(20))
-console.log(newState.volume === 20)
-
-// Напишите вместо XXX правильную строку кода, чтобы изменить громкость трека и увидеть в консоли true.
+// Какой тип должен быть вместо XXX?
