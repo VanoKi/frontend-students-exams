1da67a8c0b2d9688d9d78c5c949cb47d4c992573
VanoKi <kirsvano@gmail.com>
2025-07-31 18:56:51 +0300

    feat: 3.2.5

--- App.tsx
+++ App.tsx
@@ -1,20 +1,52 @@
+import { Provider, useSelector } from "react-redux"
+import { createRoot } from "react-dom/client"
 import { combineReducers, configureStore } from "@reduxjs/toolkit"
 
-let initialState = { items: [{ name: "Dimych" }, { name: "Ignat" }] }
+let initialState = {
+  items: [
+    { id: 1, name: "Dimych" },
+    { id: 2, name: "Ignat" },
+  ],
+}
 const usersReducer = (state = initialState, action: any) => {
   return state
 }
 
+let authInitialState = { login: "Margo", settings: { theme: "dark" } }
+const authReducer = (state = authInitialState, action: any) => {
+  return state
+}
+
+const rootReducer = combineReducers({
+  users: usersReducer,
+  auth: authReducer,
+})
+
 const store = configureStore({
-  reducer: combineReducers({
-    users: usersReducer,
-  }),
+  reducer: rootReducer,
 })
 
-store.XXX(() => {
-  console.log("state changed")
-})
+type RootStateType = ReturnType<typeof rootReducer>
 
-store.dispatch({ type: "ANY" })
+export const selector = (state: RootStateType) => state.users.items
 
-// Что нужно написать вместо XXX, чтобы в консоли увидеть 'state changed'?
+const Users = () => {
+  const users = useSelector(selector)
+
+  return (
+    <ul>
+      {users.map((u) => (
+        <li key={u.id}>{u.name}</li>
+      ))}
+    </ul>
+  )
+}
+
+createRoot(document.getElementById("root")!).render(
+  <Provider store={store}>
+    <Users />
+  </Provider>,
+)
+
+// Что нужно написать вместо XXX, чтобы отрендерить список юзеров?
+// ❗ Ответ дать минимально возможным объёмом кода
