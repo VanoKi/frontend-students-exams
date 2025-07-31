acdff37dc6f53d74ca1bfa8369172b489c919d3a
VanoKi <kirsvano@gmail.com>
2025-07-31 18:58:00 +0300

    feat: 3.2.6

--- App.tsx
+++ App.tsx
@@ -1,52 +1,7 @@
-import { Provider, useSelector } from "react-redux"
-import { createRoot } from "react-dom/client"
-import { combineReducers, configureStore } from "@reduxjs/toolkit"
+const myFunction = (a: number, b = 3) => a + b
 
-let initialState = {
-  items: [
-    { id: 1, name: "Dimych" },
-    { id: 2, name: "Ignat" },
-  ],
-}
-const usersReducer = (state = initialState, action: any) => {
-  return state
-}
+const result = myFunction(346, undefined) % 2
 
-let authInitialState = { login: "Margo", settings: { theme: "dark" } }
-const authReducer = (state = authInitialState, action: any) => {
-  return state
-}
+export const winner = result ? "Vladimir" : "Ekaterina"
 
-const rootReducer = combineReducers({
-  users: usersReducer,
-  auth: authReducer,
-})
-
-const store = configureStore({
-  reducer: rootReducer,
-})
-
-type RootStateType = ReturnType<typeof rootReducer>
-
-export const selector = (state: RootStateType) => state.users.items
-
-const Users = () => {
-  const users = useSelector(selector)
-
-  return (
-    <ul>
-      {users.map((u) => (
-        <li key={u.id}>{u.name}</li>
-      ))}
-    </ul>
-  )
-}
-
-createRoot(document.getElementById("root")!).render(
-  <Provider store={store}>
-    <Users />
-  </Provider>,
-)
-
-// Что нужно написать вместо XXX, чтобы отрендерить список юзеров?
-// ❗ Ответ дать минимально возможным объёмом кода
+// Какое значение получит переменная winner?
