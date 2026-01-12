f728d09aad13f6ee29cffabd4e363c770182072f
VanoKi <kirsvano@gmail.com>
2026-01-12 15:59:59 +0300

    feat: 3.3.3 (2026-01-12 15:59:59)

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,74 @@
-export const reducer = (state: any, action: any) => {
+import { Provider, useDispatch, useSelector } from "react-redux"
+import { createRoot } from "react-dom/client"
+import { configureStore } from "@reduxjs/toolkit"
+
+const listItemStyles = {
+  width: "100px",
+  borderBottom: "1px solid gray",
+  cursor: "pointer",
+}
+
+const students = {
+  students: [
+    { id: 1, name: "Bob" },
+    { id: 2, name: "Alex" },
+    { id: 3, name: "Donald" },
+    { id: 4, name: "Ann" },
+  ],
+}
+
+type RemoveStudentAT = {
+  type: "REMOVE-STUDENT"
+  id: number
+}
+
+export const removeStudentAC = (id: number): RemoveStudentAT => ({
+  type: "REMOVE-STUDENT",
+  id,
+})
+
+const studentsReducer = (state = students, action: RemoveStudentAT) => {
   switch (action.type) {
-    case "TRACK-ADDED":
-      return XXX
+    case "REMOVE-STUDENT":
+      return {
+        ...state,
+        students: state.students.filter((s) => s.id !== action.id),
+      }
     default:
       return state
   }
 }
 
-const addTrackAC = (trackId: number) => ({ type: "TRACK-ADDED", trackId })
+const store = configureStore({ reducer: studentsReducer })
+type RootStateType = ReturnType<typeof studentsReducer>
 
-const state = [
-  { id: 12, likesCount: 10 },
-  { id: 14, likesCount: 2 },
-  { id: 100, likesCount: 0 },
-]
-const newState = reducer(state, addTrackAC(300))
+const StudentList = () => {
+  const students = useSelector((state: RootStateType) => state.students)
 
-console.log(newState[3].likesCount === 0)
+  const dispatch = useDispatch()
 
-// Что нужно написать вместо XXX, чтобы трек корректно добавился и в консоли увидеть true?
+  return (
+    <ol>
+      {students.map((s) => {
+        const removeStudent = () => {
+          // XXX(YYY(ZZZ))
+        }
+
+        return (
+          <li key={s.id} style={listItemStyles} onClick={removeStudent}>
+            {s.name}
+          </li>
+        )
+      })}
+    </ol>
+  )
+}
+
+createRoot(document.getElementById("root")!).render(
+  <Provider store={store}>
+    <StudentList />
+  </Provider>,
+)
+
+// Что нужно написать вместо XXX, YYY и ZZZ, чтобы при клике по имени студент удалялся из списка?
+// Ответ дайте через пробел, например: doc cat fish
