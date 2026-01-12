522876ae726e8e75aa07b6781b589132e9bc896c
VanoKi <kirsvano@gmail.com>
2026-01-12 16:02:27 +0300

    feat: 3.3.2 (2026-01-12 16:02:27)

--- src/main.tsx
+++ src/main.tsx
@@ -1,74 +1,8 @@
-import { Provider, useDispatch, useSelector } from "react-redux"
-import { createRoot } from "react-dom/client"
-import { configureStore } from "@reduxjs/toolkit"
+const numbers = [96, 73, 17, 67]
+const mapFunction = (el: number, index: number) => el * index
+const mappedArray = numbers.map(mapFunction)
 
-const listItemStyles = {
-  width: "100px",
-  borderBottom: "1px solid gray",
-  cursor: "pointer",
-}
+const myNumber = mappedArray[0] && mappedArray[mappedArray.length - 1]
+export const bigCount = 25 + myNumber
 
-const students = {
-  students: [
-    { id: 1, name: "Bob" },
-    { id: 2, name: "Alex" },
-    { id: 3, name: "Donald" },
-    { id: 4, name: "Ann" },
-  ],
-}
-
-type RemoveStudentAT = {
-  type: "REMOVE-STUDENT"
-  id: number
-}
-
-export const removeStudentAC = (id: number): RemoveStudentAT => ({
-  type: "REMOVE-STUDENT",
-  id,
-})
-
-const studentsReducer = (state = students, action: RemoveStudentAT) => {
-  switch (action.type) {
-    case "REMOVE-STUDENT":
-      return {
-        ...state,
-        students: state.students.filter((s) => s.id !== action.id),
-      }
-    default:
-      return state
-  }
-}
-
-const store = configureStore({ reducer: studentsReducer })
-type RootStateType = ReturnType<typeof studentsReducer>
-
-const StudentList = () => {
-  const students = useSelector((state: RootStateType) => state.students)
-
-  const dispatch = useDispatch()
-
-  return (
-    <ol>
-      {students.map((s) => {
-        const removeStudent = () => {
-          // XXX(YYY(ZZZ))
-        }
-
-        return (
-          <li key={s.id} style={listItemStyles} onClick={removeStudent}>
-            {s.name}
-          </li>
-        )
-      })}
-    </ol>
-  )
-}
-
-createRoot(document.getElementById("root")!).render(
-  <Provider store={store}>
-    <StudentList />
-  </Provider>,
-)
-
-// Что нужно написать вместо XXX, YYY и ZZZ, чтобы при клике по имени студент удалялся из списка?
-// Ответ дайте через пробел, например: doc cat fish
+// Какое значение получит переменная bigCount?
