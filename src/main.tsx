07c6d456560794482b5856d3bfae9220c7504728
VanoKi <kirsvano@gmail.com>
2026-01-12 16:10:34 +0300

    feat: 3.3.6 (2026-01-12 16:10:34)

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,54 @@
-const clothes = {
-  name: "Margo",
-  age: 94,
-  technologies: ["HTML", "", "REACT"],
+import axios from "axios"
+import { createRoot } from "react-dom/client"
+import { useEffect, useState } from "react"
+
+// Types
+type Todolist = {
+  id: string
+  tile: string
+  order: number
+  createdAt: string
+  updatedAt: string
+  complete: boolean
 }
 
-export const [cookie = "FOO", reputation = "BOO", team = "DOO"] = clothes.technologies
+// Api
+const instance = axios.create({ baseURL: "https://exams-frontend.kimitsu.it-incubator.io/api/" })
 
-if (!XXX) {
-  alert("HEY")
+const todosAPI = {
+  getTodos() {
+    return instance.get<Todolist[]>("todos")
+  },
 }
 
-// Какую переменную следует указать вместо XXX, что бы была вызвана функция alert?
+// App
+const App = () => {
+  const [todos, setTodos] = useState<Todolist[]>([])
+
+  useEffect(() => {
+    todosAPI.getTodos().then((res) => setTodos(res.data))
+  }, [])
+
+  return (
+    <>
+      <h2>✅ Список тудулистов</h2>
+      {todos.map((t) => {
+        return (
+          <div style={t.complete ? { color: "grey" } : {}} key={t.id}>
+            <input type="checkbox" checked={t.complete} />
+            <b>Описание</b>: {t.tile}
+          </div>
+        )
+      })}
+    </>
+  )
+}
+
+createRoot(document.getElementById("root")!).render(<App />)
+
+// 📜 Описание:
+// При написании типизации по невнимательности было допущено несколько ошибок.
+// Напишите через пробел правильные свойства Todolist, в которых была допущена ошибка.
+// 💡 Debugger / network / документация в помощь
+
+// 🖥 Пример ответа: id age
