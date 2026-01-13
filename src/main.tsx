e2c962ec99b7cc4893e581d6ffc911981ee61bf3
VanoKi <kirsvano@gmail.com>
2026-01-13 19:06:06 +0300

    feat: 3.4.5 (2026-01-13 19:06:06)

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,30 @@
-const students = [{ name: "Bob" }, { name: "Alex" }, { name: "Donald" }]
-const filteredStudents = students.filter((s) => s.name !== "Kate")
-const lengthDiff = students.length - filteredStudents.length
-export const newValue = (lengthDiff && 98.21) || (11.83 && 55.64)
+export const reducer = (state: any, action: any) => {
+  switch (action.type) {
+    case "USER-NAME-UPDATED":
+      return XXX
 
-// Какое значение получит переменная newValue?
+    default:
+      return state
+  }
+}
+
+const updateUserNameAC = (name: string) => ({ type: "USER-NAME-UPDATED", name })
+
+const state = {
+  count: 10,
+  user: {
+    name: "Dimych",
+    age: 18,
+    isMarried: true,
+    status: "offline",
+  },
+  books: ["you don't know JS"],
+}
+const newState = reducer(state, updateUserNameAC("Dmitry"))
+
+console.log(newState.user.name === "Dmitry")
+console.log(newState.books === state.books)
+console.log(newState.user !== state.user)
+
+// Что нужно написать вместо XXX, чтобы корректно обновить имя пользователя
+// и в консоли увидеть: true true true?
