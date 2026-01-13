2138da063c0508e483f3214d416bc6b1171b192a
VanoKi <kirsvano@gmail.com>
2026-01-13 19:27:03 +0300

    feat: 3.t.6 (2026-01-13 19:27:03)

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,72 @@
-export const reducer = (state: any, action: any) => {
+import { CSSProperties } from "react"
+import { createRoot } from "react-dom/client"
+
+const commonStyles: CSSProperties = {
+  border: "1px solid black",
+  margin: "100px auto",
+  width: "300px",
+  height: "150px",
+  textAlign: "center",
+}
+
+const btnStyles: CSSProperties = {
+  color: "white",
+  fontWeight: "bold",
+  backgroundColor: "darkgray",
+  borderRadius: "3px",
+  minWidth: "40px",
+}
+
+const changeCounter = (state: number, action: any): number => {
   switch (action.type) {
-    case "TRACK-DELETED":
-      return state.filter((track: any) => track.id !== action.trackId)
+    case "INC_VALUE":
+      return state + 1
+    case "RESET":
+      return 0
+    case "DEC_VALUE":
+      return state - 1
     default:
       return state
   }
 }
 
-const deleteTrackAC = (trackId: number) => ({ type: "TRACK-DELETED", trackId })
+const Counter = () => {
+  const [value, setValue] = XXX(changeCounter, 0)
+  const [isCounter, setIsCounter] = YYY(true)
 
-const state = [
-  { id: 12, likesCount: 10 },
-  { id: 14, likesCount: 2 },
-  { id: 100, likesCount: 0 },
-]
-const newState = reducer(state, deleteTrackAC(14))
+  return (
+    <div style={commonStyles}>
+      {isCounter && (
+        <div>
+          <div style={{ marginBottom: "20px" }}>
+            <h2>{value}</h2>
+            <button style={{ ...btnStyles, backgroundColor: "red" }} onClick={() => setIsCounter(false)}>
+              OFF
+            </button>
+          </div>
+          <button style={btnStyles} onClick={() => setValue({ type: "INC_VALUE" })}>
+            +
+          </button>
+          <button style={btnStyles} onClick={() => setValue({ type: "RESET" })}>
+            0
+          </button>
+          <button style={btnStyles} onClick={() => setValue({ type: "DEC_VALUE" })}>
+            -
+          </button>
+        </div>
+      )}
+      {!isCounter && (
+        <div style={{ textAlign: "center" }}>
+          <h2>Counter not working</h2>
+          <button style={{ ...btnStyles, backgroundColor: "green" }} onClick={() => setIsCounter(true)}>
+            ON
+          </button>
+        </div>
+      )}
+    </div>
+  )
+}
 
-console.log(newState.length === 2)
+createRoot(document.getElementById("root")!).render(<Counter />)
 
-// Что нужно написать вместо XXX, чтобы корректно удалить трек и в консоли увидеть true?
+// Что надо написать вместо XXX и YYY, чтобы код работал? Напишите через пробел.
