3db79b906abae9076a685f04ac4da2b0289a59c9
VanoKi <kirsvano@gmail.com>
2026-01-13 19:23:56 +0300

    feat: 3.t.5 (2026-01-13 19:23:56)

--- src/main.tsx
+++ src/main.tsx
@@ -1,74 +1,21 @@
-import { Provider, useDispatch, useSelector } from "react-redux"
-import { createRoot } from "react-dom/client"
-import { configureStore } from "@reduxjs/toolkit"
-
-const listItemStyles = {
-  width: "100px",
-  borderBottom: "1px solid gray",
-  cursor: "pointer",
-}
-
-const students = {
-  students: [
-    { id: 1, name: "Bob" },
-    { id: 2, name: "Alex" },
-    { id: 3, name: "Donald" },
-    { id: 4, name: "Ann" },
-  ],
-}
-
-type RemoveStudentAT = {
-  type: "REMOVE-STUDENT"
-  id: number
-}
-
-export const removeStudentAC = (id: number): RemoveStudentAT => ({
-  type: "REMOVE-STUDENT",
-  id,
-})
-
-const studentsReducer = (state = students, action: RemoveStudentAT) => {
+export const reducer = (state: any, action: any) => {
   switch (action.type) {
-    case "REMOVE-STUDENT":
-      return {
-        ...state,
-        students: state.students.filter((s) => s.id !== action.id),
-      }
+    case "TRACK-DELETED":
+      return state.filter((track: any) => XXX)
     default:
       return state
   }
 }
 
-const store = configureStore({ reducer: studentsReducer })
-type RootStateType = ReturnType<typeof studentsReducer>
+const deleteTrackAC = (trackId: number) => ({ type: "TRACK-DELETED", trackId })
 
-const StudentList = () => {
-  const students = useSelector((state: RootStateType) => state.students)
+const state = [
+  { id: 12, likesCount: 10 },
+  { id: 14, likesCount: 2 },
+  { id: 100, likesCount: 0 },
+]
+const newState = reducer(state, deleteTrackAC(14))
 
-  const dispatch = useDispatch()
+console.log(newState.length === 2)
 
-  return (
-    <ol>
-      {students.map((s) => {
-        const removeStudent = () => {
-          // XXX(YYY(ZZZ))
-        }
-
-        return (
-          <li key={s.id} style={listItemStyles} onClick={removeStudent}>
-            {s.name}
-          </li>
-        )
-      })}
-    </ol>
-  )
-}
-
-createRoot(document.getElementById("root")!).render(
-  <Provider store={store}>
-    <StudentList />
-  </Provider>,
-)
-
-// Что нужно написать вместо XXX, YYY и ZZZ, чтобы при клике по имени студент удалялся из списка?
-// Ответ дайте через пробел, например: doc cat fish
+// Что нужно написать вместо XXX, чтобы корректно удалить трек и в консоли увидеть true?
